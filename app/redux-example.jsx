bc261f12a7dee6f2598ca685ca06ad7c0727f160
Mayank Joshi <mayank@aisle.co>
2018-05-03 00:03:11 +0530

    changes

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -1,4 +1,11 @@
 var redux = require('redux');
 console.log('starting redux');
 
-var store = redux.createStore();
+var reducer = (state = {name: 'Anonymous'}, action) => {
+    // state = state || {name: 'Anonymous'};
+    return state;
+};
+var store = redux.createStore(reducer);
+
+var currentState = store.getState();
+console.log('currentState', currentState);
